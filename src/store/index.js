97bea1b7f3f50733c2b7354af956ab3ca8ad5059
18222478537@163.com <18222478537@163.com>
2019-09-04 17:08:15 +0800

    创建store

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+// 基础store创建流程
+// import {createStore} from 'redux' 
+// const store = createStore() 
+// export default store
+
+
+import {createStore} from 'redux' 
+
+const store = createStore() 
+
+export default store
